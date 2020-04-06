7ae10cd02f9cf7e5625824f21d9fa420cc887652
Phong <phong2a63@gmail.com>
2020-04-06 09:19:32 +0700

    new Dashboard

---
+++ src/containers/DashBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="dash-board">
+    <TopMenu />
+  </div>
+</template>
+
+<script>
+import TopMenu from "../components/topmenu/TopMenu";
+export default {
+  components: {
+    TopMenu
+  }
+};
+</script>
+
+<style scoped>
+</style>
